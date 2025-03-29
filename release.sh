6b0a2705cf52bef927384d505ddf24bbca2c839c
Alexandru Branza <branza.alex@gmail.com>
2025-03-29 08:21:23 +0200

    Fix Release Script

--- release.sh
+++ release.sh
@@ -1,2 +1,3 @@
 docker manifest rm jaruba/rpdb-folders-docker:latest
 docker manifest create jaruba/rpdb-folders-docker:latest jaruba/rpdb-folders-docker-arm64:latest jaruba/rpdb-folders-docker-amd64:latest
+docker manifest push jaruba/rpdb-folders-docker:latest
